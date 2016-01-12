f052f9b226eb25c123c0c11d9be9243891e608bf
Wayne Shan <iWayneShan@gmail.com>
2016-01-12 17:13:12 -0500

    Refresh after solve the problem. Avoid the reverse order from the original order. Add other restriction for random order next time

--- PuzzleTimeObjective-C/PuzzleTime/GameViewController.m
+++ PuzzleTimeObjective-C/PuzzleTime/GameViewController.m
@@ -19,23 +19,50 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [_collectionView registerNib:[UINib nibWithNibName:@"PuzzleCell" bundle:nil] forCellWithReuseIdentifier:@"Cell"];
-    //Initialize
+    [self resetTheGame];
+}
+
+- (void) resetTheGame {
+    //Initialize with Given value
     _imageNamePostfix = @".jpg";
     _imageNamePrefix = @"number";
-    _numberOfCells = 9;
+    _numberOfCells = 4;
     _numberPerRow = (sqrt(_numberOfCells));
+    
+    
+    //build arrays
+    [self buildOriItems];
+    [self buildRandomItems];
+}
+
+- (void) buildOriItems {
     _oriOrdered = [[NSMutableArray alloc] init];
-    _curOrdered = [[NSMutableArray alloc] init];
+    
     for (int i = 0; i < _numberOfCells - 1; i++) {
         NSString *imageName = [NSString stringWithFormat:@"%@%d%@", _imageNamePrefix, i, _imageNamePostfix];
         [_oriOrdered addObject:imageName];
     }
-    [_oriOrdered addObject:@""];
-    _curOrdered = [NSMutableArray arrayWithArray:_oriOrdered];
 }
 
-- (void)viewDidAppear:(BOOL)animated{
+- (void) buildRandomItems {
+    BOOL isValidRandom = NO;
     
+    //Avoid the reverse of the original order
+    while (!isValidRandom) {
+        _curOrdered = [[NSMutableArray alloc] init];
+        NSInteger randLocInt = 0;
+        NSMutableArray* tempItems = [NSMutableArray arrayWithArray:_oriOrdered];
+        for (NSInteger i = 0; i < _numberOfCells - 1; i++) {
+            randLocInt = arc4random() % [tempItems count];
+            [_curOrdered addObject:[tempItems objectAtIndex:randLocInt]];
+            [tempItems removeObjectAtIndex:randLocInt];
+        }
+        tempItems = [NSMutableArray arrayWithArray:[[_curOrdered reverseObjectEnumerator] allObjects]];
+        if (![tempItems isEqualToArray:_oriOrdered]) {
+            isValidRandom = YES;
+        }
+    }
+    [_curOrdered addObject:@""];
 }
 
 
@@ -43,15 +70,20 @@
     return _numberOfCells;
 }
 
+
+//Decide the size of cells
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
     
-    _widthOfCell = _collectionView.frame.size.width / (_numberPerRow + 1);
-
+    if (!_widthOfCell) {
+        _widthOfCell = _collectionView.frame.size.width / (_numberPerRow + 1);
+    }
+    
     return CGSizeMake(_widthOfCell, _widthOfCell);
 }
 
+//layout the collectionView for top/bottom/left/right paddings
 - (UIEdgeInsets)collectionView:(UICollectionView*)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    NSInteger space = _widthOfCell / (_numberPerRow + 1) * 5 / 3;
+    NSInteger space = _widthOfCell / (_numberPerRow + 1);
     
     return UIEdgeInsetsMake(space, space, space, space);
 }
@@ -65,6 +97,7 @@
 }
 
 
+//Move the cell with the empty one
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
     
     NSIndexPath *emptySpot = [NSIndexPath indexPathForItem:[_curOrdered indexOfObject:@""] inSection:0];
@@ -86,24 +119,26 @@
         [_curOrdered exchangeObjectAtIndex:indexPath.item withObjectAtIndex:emptySpot.item];
         [_collectionView reloadItemsAtIndexPaths: changedIndices];
     }
+    [self checkFinished];
 }
 
 
-- (IBAction)showPuzzleGame:(UIButton *)sender {
-//    UICollectionViewFlowLayout* flowLayout = [[UICollectionViewFlowLayout alloc]init];
-//    flowLayout.itemSize = CGSizeMake(100, 100);
-//    PuzzleCollectionViewController *puzzleController = [[PuzzleCollectionViewController alloc] initWithCollectionViewLayout:flowLayout];
-//    [self showViewController:puzzleController sender:nil];
+//Check if user solve the puzzle
+- (void) checkFinished {
+    NSMutableArray* tempItems = [NSMutableArray arrayWithArray:_oriOrdered];
+    [tempItems addObject:@""];
+    
+    if ([tempItems isEqualToArray:_curOrdered]) {
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Bingo!" message:@"You make it!" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            NSLog(@"Finished. Now reload");
+            [self resetTheGame];
+            [self.collectionView reloadData];
+            NSLog(@"Done");
+        }];
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
